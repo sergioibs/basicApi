967fa16844701b9ee6b2c97413ac0f78ba3a9965
Sergio Ibañez <b.sis@tid.es>
2016-12-06 12:08:32 +0100

    add mongo conection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var measureModel = require('./models/measure.js');
+var mongoose = require('mongoose');
 
 app.get('/', function(req, res) {
     console.log("Resquest to / recieved from: " + req.header('x-forwarded-for'));
@@ -26,8 +27,12 @@ app.get('/write', function(req, res) {
     res.send('Hello World');
 })
 
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
 var server = app.listen(port, function() {
     var port = server.address().port
-
-    console.log("Example app listening at http://localhost:%s", port)
+    console.log("App listening at http://localhost:%s", port)
 })
